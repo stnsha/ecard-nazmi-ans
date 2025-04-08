5c062abee126bff959c5f9f21b71ede3792e9cfb
Siti Anasuha <anasuharosli@gmail.com>
2025-04-09 00:50:45 +0800

    fix: rsvp migration

--- database/migrations/2025_03_08_083146_create_rsvp_table.php
+++ database/migrations/2025_03_08_083146_create_rsvp_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('RSVP', function (Blueprint $table) {
+        Schema::create('rsvp', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->boolean('is_attend')->default(true);
