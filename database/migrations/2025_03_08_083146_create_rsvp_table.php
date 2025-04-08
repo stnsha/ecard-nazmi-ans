4f2343ae42b9fbc216326b315fca7ec9f365fce5
Siti Anasuha <anasuharosli@gmail.com>
2025-04-09 00:52:43 +0800

    fix: rsvp migration

--- database/migrations/2025_03_08_083146_create_rsvp_table.php
+++ database/migrations/2025_03_08_083146_create_rsvp_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->boolean('is_attend')->default(true);
-            $table->string('time_slot');
+            $table->integer('pax')->default(0); // Add pax column
+            $table->unsignedBigInteger('time_slot'); // time_slot as unsignedBigInteger
             $table->longText('wishes');
             $table->softDeletes();
             $table->timestamps();
+
+            // Add foreign key constraint to time_slot column
+            $table->foreign('time_slot')->references('id')->on('slots');
         });
     }
 
