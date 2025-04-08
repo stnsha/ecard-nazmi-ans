4231b03697a3d7fbc141014c79f505950436c6ba
Siti Anasuha <anasuharosli@gmail.com>
2025-04-09 01:00:12 +0800

    fix: rsvp migration

--- database/migrations/2025_04_05_004929_update_rsvp_table.php
+++ database/migrations/2025_04_05_004929_update_rsvp_table.php
@@ -8,30 +8,27 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // First, drop the existing foreign key and column
         Schema::table('rsvp', function (Blueprint $table) {
-            $table->dropForeign(['time_slot']); // This drops any foreign key associated with the `time_slot` column
+            // $table->dropForeign(['time_slot']);
             $table->dropColumn('time_slot');
         });
 
-        // Now, re-add the `time_slot` column and the foreign key constraint
         Schema::table('rsvp', function (Blueprint $table) {
             $table->unsignedBigInteger('time_slot');
             $table->foreign('time_slot')->references('id')->on('slots');
+
             $table->integer('pax')->default(0);
         });
     }
 
     public function down(): void
     {
-        // In the down method, reverse the changes: drop the foreign key and column
         Schema::table('rsvp', function (Blueprint $table) {
             $table->dropForeign(['time_slot']);
             $table->dropColumn('time_slot');
             $table->dropColumn('pax');
         });
 
-        // Re-add the original `time_slot` column and foreign key constraint
         Schema::table('rsvp', function (Blueprint $table) {
             $table->unsignedBigInteger('time_slot');
             $table->foreign('time_slot')->references('id')->on('slots');
